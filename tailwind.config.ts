29ebfdf7f8b13187aa82dbfa19e5c33c7ed958a6
Mahesh Krishnan <maheshkrishnan1242@gmail.com>
2026-03-27 20:22:11 +0530

    feat: setup color palette in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+import fluid, { screens, fontSize } from "fluid-tailwind";
 
 const config: Config = {
-  content: {
-    files: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-    extract,
-  },
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+
   theme: {
     extend: {
       fontSize,
@@ -13,10 +11,25 @@ const config: Config = {
         sml: "20rem",
         ...screens,
       },
-      fontFamily: {},
-      colors: {},
+      colors: {
+        bg: "#0B1326",
+        text: "#C7C4D7",
+        primary: "#1000A9",
+        secondary: "#64748B",
+        bigText: "#DAE2FD",
+        primaryLight: "#C0C1FF",
+        lightOrange: "#FFB783",
+        clearityLow: "#FFB4AB",
+        recording: "#FFB4AB",
+        recordingBg: "rgba(147, 0, 10, 0.2)",
+      },
+      backgroundImage: {
+        linear: "linear-gradient(90deg, #C0C1FF 0%, #8083FF 100%)",
+      },
     },
   },
+
   plugins: [fluid],
 };
+
 export default config;
